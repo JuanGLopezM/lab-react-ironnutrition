ec805c9d1f6c97886554112df4c570592aa81cad
Juan G Lopez <nahujg@gmail.com>
2022-07-13 17:55:12 +0200

    iteración 2
    iteración 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+import { useState } from "react";
+// src/index.js
+import 'antd/dist/antd.css';
+// ejemplo import antd
+// import { Card, Row, Col, Divider, Input, Button } from 'antd';
+import foods from './foods.json';
+import FoodBox from "./components/FoodBox";
+import {Divider} from 'antd'
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [listFoods, setlistFoods] = useState (foods);
+  return <div className="App">
+  <Divider>FOOD LIST</Divider>
+  <FoodBox food={ {
+  name: "Orange",
+  calories: 85,
+  image: "https://i.imgur.com/abKGOcv.jpg",
+  servings: 1
+}} />
+    {listFoods.map((food) => {
+        return  <div>
+      <p> {food.name} </p>
+      <img src={food.image} width={300} alt="comida"/>
+      <p> </p>
+    </div>;
+      })}
+  </div>;
 }
-
 export default App;
